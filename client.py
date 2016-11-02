bd49a3cdc5520b7dbac4b0916843aecdb3e614bf
davidheg <david.hegarty.9@gmail.com>
2016-11-02 15:38:08 +0000

    client now prints the response headers

--- client.py
+++ client.py
@@ -5,9 +5,13 @@ httpServ = httplib.HTTPConnection("10.62.0.213", 8000)
 httpServ.connect()
 
 httpServ.request('GET', "/echo.php?message=hi")
+
 response = httpServ.getresponse()
 if response.status == httplib.OK:
     print "Response from server"
     print response.read()
+	headers  = response.getheaders()
+    for header in headers:
+        print header
 
 httpServ.close()
